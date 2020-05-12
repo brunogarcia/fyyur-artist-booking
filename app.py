3b35dd860a0f537add271ee4f13e695c6e0bd822
Bruno Garcia <bruno@garciaechegaray.com>
2020-05-12 12:31:23 +0200

    List artists

--- app.py
+++ app.py
@@ -321,16 +321,10 @@ def delete_venue(venue_id):
 @app.route('/artists')
 def artists():
     # TODO: replace with real data returned from querying the database
-    data = [{
-      "id": 4,
-      "name": "Guns N Petals",
-    }, {
-      "id": 5,
-      "name": "Matt Quevedo",
-    }, {
-      "id": 6,
-      "name": "The Wild Sax Band",
-    }]
+    data = Artist.query \
+      .with_entities(Artist.id, Artist.name) \
+      .order_by('id') \
+      .all()
 
     return render_template('pages/artists.html', artists=data)
 
