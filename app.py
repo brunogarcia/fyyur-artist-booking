e0e4ac04c08139740d35d9daf098d48bf5cfd218
Bruno Garcia <bruno@garciaechegaray.com>
2020-05-12 11:59:04 +0200

    The genres must be an array of strings

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ class Venue(db.Model):
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String(120)))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
 
@@ -56,7 +56,7 @@ class Artist(db.Model):
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String(120)))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
 
@@ -260,7 +260,7 @@ def create_venue_submission():
     state = request.form['state']
     address = request.form['address']
     phone = request.form['phone']
-    genres = request.form['genres']
+    genres = request.form.getlist('genres')
     facebook_link = request.form['facebook_link']
 
     try:
@@ -519,7 +519,7 @@ def create_artist_submission():
     city = request.form['city']
     state = request.form['state']
     phone = request.form['phone']
-    genres = request.form['genres']
+    genres = request.form.getlist('genres')
     facebook_link = request.form['facebook_link']
 
     try:
